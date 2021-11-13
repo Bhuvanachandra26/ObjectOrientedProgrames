9cf1650d19077170956387a35e3161f21119808e
Bhuvanachandra26 <bhuvanachandra26@gmail.com>
2021-11-13 13:10:43 +0530

    added AccountCreated

--- StockReport.java
+++ StockReport.java
@@ -1,52 +1,21 @@
-package com.Bridgelabz.ObjectOrientedprogrames;
-
-import com.Bridgelabz.Objectorientedprograms.StockAccountManagement;
+package com.Bridgelabz.ObjectOrientedPrograms;
+import StockModel.Stock;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class StockReport {
-	private String shareName;
-	private double noOfStocks;
-	private static double noOfShares;
-	private static double sharePrice;
-	public double getNoOfShares() {
-		return noOfShares;
-	}
-	public void setNoOfShares(double noOfShares) {
-		this.noOfShares = noOfShares;
-	}
-	public String getShareName() {
-		return shareName;
-	}
-	public void setShareName(String shareName) {
-		this.shareName = shareName;
-	}
+    Scanner scanner = new Scanner(System.in);
+    public void createStockAccount(HashMap<String, HashMap<String, Long>> accountList) {
+        System.out.println("Enter a account name: ");
+        String accountHolderName = scanner.next();
+        if (accountList.containsKey(accountHolderName)) {
+            System.out.println("Account exists.");
+        } else {
 
-public static void main(String[] args) {
-	StockAccountManagement name = new StockAccountManagement();
-	name.setShareName("bhuvana");
-	name.setNoOfShares(4000);
-	name.setNoOfStocks(5);
-	name.setSharePrice(10);
-	System.out.println(name.getShareName());
-	System.out.println(name.getNoOfShares());
-	System.out.println(name.getNoOfStocks());
-	System.out.println(name.getSharePrice());
-	@SuppressWarnings("unused")
-	double totalvalue = noOfShares * sharePrice;
-	System.out.println("totalvalue" + totalvalue);
-	
-	
+            HashMap<String, Long> accountHolderInfo = new HashMap<String, Long>();     // "accountHolderInfo" contains a particular account holder info
 
-	}
-	public double getNoOfStocks() {
-		return noOfStocks;
-	}
-	public void setNoOfStocks(double noOfStocks) {
-		this.noOfStocks = noOfStocks;
-	}
-	public double getSharePrice() {
-		return sharePrice;
-	}
-	public void setSharePrice(double sharePrice) {
-		this.sharePrice = sharePrice;
-	}
-}
+            accountList.put(accountHolderName, new HashMap<String, Long>());
+            System.out.println("Account created.");
+        }
+    }
